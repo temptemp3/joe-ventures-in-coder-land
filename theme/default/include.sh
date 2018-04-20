8fe24118b174d5d3c6b9363d113282c400aa6614
Nicholas M Shellabarger <nicholas@h-maneki.net>
2018-04-20 23:07:46 +0900

    add ga 1524233266

--- theme/default/include.sh
+++ theme/default/include.sh
@@ -23,6 +23,14 @@ $( if-link-canonical )
 body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 </style>
 $( if-hljs )
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-104620931-1', 'auto');
+  ga('send', 'pageview');
+</script>
 </head>
 EOF
 }
